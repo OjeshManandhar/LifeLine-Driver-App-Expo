8898e6dc162ea635f062d4cfb9e6c191dc409f9a
OjeshManandhar <ozes.manandhar@gmail.com>
2020-09-10 11:54:40 +0545

    Absolute path using jsconfig.json

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            assets: './src/assets',
-            screens: './src/screens',
-            navigator: './src/navigator',
-            components: './src/components'
-          }
-        }
-      ]
-    ]
+    presets: ['babel-preset-expo']
   };
 };
