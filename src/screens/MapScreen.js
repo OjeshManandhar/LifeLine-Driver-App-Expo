107d302458de5664520a949b93aa3149d4d2a1dd
OjeshManandhar <ozes.manandhar@gmail.com>
2020-09-10 10:53:17 +0545

    Navigate to Login if user log outs

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,19 +1,52 @@
-import React, { useEffect } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useEffect, useCallback } from 'react';
+import {
+  View,
+  Text,
+  Alert,
+  Button,
+  StyleSheet,
+  BackHandler
+} from 'react-native';
 
 function MapScreen({ navigation, route }) {
   const routeParams = route.params;
 
-  // Prevent going back to Login
+  // Logout alert
   useEffect(() => {
-    navigation.addListener('beforeRemove', (e) => e.preventDefault());
+    navigation.addListener('beforeRemove', e => {
+      e.preventDefault();
+
+      Alert.alert('Log out?', 'Are you sure you want to Log out?', [
+        { text: 'No', style: 'cancel', onPress: () => {} },
+        {
+          text: 'Yes',
+          style: 'destructive',
+          // If the user confirmed, then we dispatch the action we blocked earlier
+          // This will continue the action that had triggered the removal of the screen
+          onPress: () => navigation.dispatch(e.data.action)
+        }
+      ]);
+    });
   }, [navigation]);
+
+  const handleBackButton = useCallback(() => {
+    return true;
+  }, []);
+
+  // Handling the Hardware Back button
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+    };
+  }, [handleBackButton]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Map Screen</Text>
       <Text style={styles.text}>{JSON.stringify(routeParams)}</Text>
-      <Button title='To Login' onPress={() => navigation.navigate('Login')} />
+      <Button title='Log out' onPress={() => navigation.navigate('Login')} />
     </View>
   );
 }
