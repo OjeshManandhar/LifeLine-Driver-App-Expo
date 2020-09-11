a612c30fb6bbb239423dae3a7a10e7d5c16596b0
OjeshManandhar <ozes.manandhar@gmail.com>
2020-09-11 11:30:59 +0545

    Modify Status Bar

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 // navigator
 import Navigator from 'navigator/Navigator';
 
 function App() {
-  return <Navigator />;
+  return (
+    <React.Fragment>
+      <Navigator />
+      <StatusBar translucent={false} />
+    </React.Fragment>
+  );
 }
 
 export default App;
