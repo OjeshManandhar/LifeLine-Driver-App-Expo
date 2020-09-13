abc46e00f0c0584727cf632e3d74da558a4a6130
OjeshManandhar <ozes.manandhar@gmail.com>
2020-09-13 13:16:30 +0545

    Add a global file for constant strings

---
+++ src/global/strings.js
@@ -0,0 +1,14 @@
+export const Login = {
+  loginForm: {
+    phoneNumber: 'Phone Number',
+    password: 'Password'
+  },
+  signUp: {
+    title: 'Sign Up',
+    detail: 'Please contact the admin to register you to the App. \nThank You!'
+  },
+  errorText: {
+    noNetwork: 'Please check your internet connection',
+    phonePassError: 'Phone number and Password does not match'
+  }
+};
