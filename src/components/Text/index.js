008754d650a0c02bb098f1eca9ab362a07e75ace
OjeshManandhar <ozes.manandhar@gmail.com>
2020-09-14 14:32:32 +0545

    Create a custom Text component

---
+++ src/components/Text/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text as T } from 'react-native';
+
+// global
+import { WorkSansRegular } from 'global/styles';
+
+function Text({ style, children, ...props }) {
+  return <T style={[style, WorkSansRegular]}>{children}</T>;
+}
+
+export default Text;
